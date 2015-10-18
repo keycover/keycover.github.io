1eeaeb8893a8bd404cccf7d7b465792862272ba6
Riccardo Amadio <laemaildiriccardo@gmail.com>
2015-10-18 23:41:53 +0200

    well

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,14 +1,18 @@
 function adapt_image(){
-  var h=$("img[src='img/face/elio.jpg']").height();
-  var w=$("img[src='img/face/elio.jpg']").width();
+  var h=$("img[src='img/face/riccardo.jpg']").height();
+  var w=$("img[src='img/face/riccardo.jpg']").width();
   $("img[src='img/face/riccardo.jpg']").width(w).height(h);
   $("img[src='img/face/federico.png']").width(w).height(h);
 $("img[src='img/face/empty.jpg']").eq(0).width(w).height(h);
 $("img[src='img/face/empty.jpg']").eq(1).width(w).height(h);
+$("img[src='img/face/elio.jpg']").width(w).height(h);
+
 }
+
 $(function(){
 
 $(window).resize(function(){
+//when page size change
 adapt_image();
 });
 
